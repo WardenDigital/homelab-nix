c08e99c9a17added1b655f22323cdeacf738913d
WardenDigital <wardendigital@proton.me>
2026-04-28 21:54:08 +0200

    upd

--- hosts/ankylo/home.nix
+++ hosts/ankylo/home.nix
@@ -1,28 +1,20 @@
-{ pkgs, ... }:
+{ ... }:
 {
   home.stateVersion = "25.11";
   home.username = "warden_digital";
   home.homeDirectory = "/home/warden_digital";
 
   imports = [
-    ./../../modules/home/wm/hyprland.nix
     ./home/cli.nix
     ./home/terminal.nix
-  ];
-
-  home.packages = with pkgs; [
-    imagemagick
-    ghostscript
-    tectonic
-    vscode
   ];
 
   programs.git = {
     enable = true;
     settings = {
       user = {
-        name = "WardenDigital";
-        email = "wardendigital@proton.me";
+        name = "homelab-ankylo";
+        email = "homelab+wardendigital@proton.me";
       };
       pull = {
         rebase = false;
