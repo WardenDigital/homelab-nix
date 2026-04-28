a574f907dadb5d107c49f1caa2cbd21b1c317ec1
WardenDigital <wardendigital@proton.me>
2026-04-28 22:04:24 +0200

    battery saver

--- hosts/ankylo/configurations/laptop.nix
+++ hosts/ankylo/configurations/laptop.nix
@@ -1,13 +1,23 @@
 { ... }:
 {
-  services.logind.settings.Login = {
-    HandleLidSwitchExternalPower = "ignore";
-    HandleLidSwitchDocked = "ignore";
-    HandleLidSwitch = "ignore";
-  };
-
   # Blackout screen after 60 seconds
   boot.kernelParams = [ "consoleblank=60" ];
   # Termal control daemon
-  services.thermald.enable = true;
+  services = {
+    logind.settings.Login = {
+      HandleLidSwitchExternalPower = "ignore";
+      HandleLidSwitchDocked = "ignore";
+      HandleLidSwitch = "ignore";
+    };
+
+    tlp = {
+      enable = true;
+      settings = {
+        # Stop charging at 80%, start again when it drops to 75%
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+      };
+    };
+    thermald.enable = true;
+  };
 }
