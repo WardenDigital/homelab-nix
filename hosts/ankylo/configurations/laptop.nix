ed5307c135d5933aadeb6599c7e7f72c41bf1ec6
WardenDigital <wardendigital@proton.me>
2026-04-28 22:00:23 +0200

    update lid switch

--- hosts/ankylo/configurations/laptop.nix
+++ hosts/ankylo/configurations/laptop.nix
@@ -1,12 +1,11 @@
 { ... }:
 {
+  services.logind.settings.Login = {
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+    HandleLidSwitch = "ignore";
+  };
 
-  services.logind.extraConfig = ''
-    # Don't suspend when the lid is closed
-    HandleLidSwitch=ignore
-    HandleLidSwitchExternalPower=ignore
-    HandleLidSwitchDocked=ignore
-  '';
   # Blackout screen after 60 seconds
   boot.kernelParams = [ "consoleblank=60" ];
   # Termal control daemon
